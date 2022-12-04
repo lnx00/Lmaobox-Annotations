7596ab062cd15828485939adb2392a2d8a47a68e
I fucking hate Rap <64323552+LewdDeveloper@users.noreply.github.com>
2022-12-05 00:11:14 +0700

    Initial Commit

---
+++ test.lua
@@ -0,0 +1,11 @@
+---@param callbackObject UserMessage
+callbacks.Register("DispatchUserMessage", function (callbackObject)
+    
+end)
+
+
+--- clientstate, filesystem, gamecoordinator, gamerules, inventory, itemschema, party, playerlist, steam, wrap, vector
+--- AttributeDefinition, Item, ItemDefinition, MatchGroup, MatchMapDefinition, PartyMemberActivity, WeaponData
+
+---@return boolean enabled If the item is enabled
+local function isEnabled() end
