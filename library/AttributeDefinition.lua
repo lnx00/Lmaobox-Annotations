0fc25b6553bbdb82a68ad1090f051dc7c67a182c
lnx00 <linch255@gmail.com>
2022-12-05 17:53:50 +0100

    Added some classes

---
+++ library/AttributeDefinition.lua
@@ -0,0 +1,22 @@
+---@meta
+
+---The AttributeDefinition object contains information about an attribute in TF2.
+---@class AttributeDefinition
+AttributeDefinition = {}
+
+---Returns the name of the attribute.
+---@return string attribute
+---@nodiscard
+function AttributeDefinition:GetName() end
+
+---Returns the ID of the attribute.
+---@return number attribute
+---@nodiscard
+function AttributeDefinition:GetID() end
+
+---Returns true if the attribute is stored as an integer. For numeric attibutes, false means it is stored as a float.
+---@return boolean
+---@nodiscard
+function AttributeDefinition:IsStoredAsInteger() end
+
+return AttributeDefinition
---
+++ library/WeaponData.lua
@@ -0,0 +1,21 @@
+---@meta
+
+---Contains variables related to specifications of a weapon, such as firing speed, number of projectiles, etc. Some of them may not be used, or may be wrong.
+---@class WeaponData
+---@field damage integer
+---@field bulletsPerShot integer
+---@field range number
+---@field spread number
+---@field punchAngle number
+---@field timeFireDelay number
+---@field timeIdle number
+---@field timeIdleEmpty number
+---@field timeReloadStart number
+---@field timeReload number
+---@field drawCrosshair number
+---@field projectile integer
+---@field ammoPerShot integer
+---@field projectileSpeed number
+---@field smackDelay number
+---@field useRapidFireCrits boolean
+WeaponData = {}
