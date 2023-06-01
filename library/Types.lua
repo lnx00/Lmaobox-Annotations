f55a926fee89c476b774aad5a8c4608856a3a8a1
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-06-01 22:58:41 +0200

    Added rich presence keys

--- library/Types.lua
+++ library/Types.lua
@@ -1,4 +1,14 @@
 ---@meta
 
+---@alias RichPresenceKey
+---| "currentmap"
+---| "connect"
+---| "steam_player_group"
+---| "steam_player_group_size"
+---| "state"
+---| "matchgrouploc"
+---| "steam_display"
+---| "status"
+
 ---@class Texture : integer
 ---@class Font : integer
