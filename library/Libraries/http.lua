97d9bd45e7d7f5f8912ffe6c5952c6e19f8860ff
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-11-25 01:56:24 +0100

    Added http library

---
+++ library/Libraries/http.lua
@@ -0,0 +1,10 @@
+---@meta
+
+---@class http
+http = {}
+
+---(Undocumented)
+---Performs a HTTP GET request to the specified URL and returns the response.
+---@param url string
+---@return string
+function http.Get(url) end
